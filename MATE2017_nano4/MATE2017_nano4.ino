946ad9f60661a8043e6e10ea00a126bb814e9020
wan40657819 <wantakyin@gmail.com>
2018-03-03 01:52:00 +0800

    17 nano4

--- MATE2017_nano4/MATE2017_nano4.ino
+++ MATE2017_nano4/MATE2017_nano4.ino
@@ -1,11 +1,22 @@
 #include <SoftwareSerial.h>
 
-
+SoftwareSerial BT(12, 11); // RX | TX
+int at = 13;
 const int pin_motor[4][2] = {{2, 3}, {4, 5}, {8, 9}, {6, 7}};
 const int pin_stepper[3][3] = {{A0, A1, A2}, {A3, A4, A5}, {}}; //dir, step, enable
-byte s_m[4] = {0, 0, 0, 0};
-byte s_s[3] = {0, 0, 0};
+byte s_m[7] = {0, 0, 0, 0, 0, 0, 0};
+byte s_s[2] = {0, 0};
 int stepperSpeed = 150;
+
+void disc() {
+  digitalWrite(at, HIGH);
+  delay(100);
+  BT.write("AT\r\n");
+  delay(100);
+  BT.write("AT+DISC\r\n");
+  delay(100);
+  digitalWrite(at, LOW);
+}
 
 void stepper(int i, byte dir) {
   if (dir == 0)
@@ -13,20 +24,20 @@ void stepper(int i, byte dir) {
   else if (dir == 1) {
     digitalWrite(pin_stepper[i][2], LOW);
     digitalWrite(pin_stepper[i][0], HIGH);
-    for (int i = 0; i < 5000; i++) {
+    for (int x = 0; x < 5000; x++) {
       digitalWrite(pin_stepper[i][1], HIGH);
       delayMicroseconds(stepperSpeed);
-      digitalWrite(pin_stepper[1], LOW);
+      digitalWrite(pin_stepper[i][1], LOW);
       delayMicroseconds(stepperSpeed);
     }
   }
   else if (dir == 2) {
     digitalWrite(pin_stepper[i][2], LOW);
     digitalWrite(pin_stepper[i][0], LOW);
-    for (int i = 0; i < 5000; i++) {
+    for (int x = 0; x < 5000; x++) {
       digitalWrite(pin_stepper[i][1], HIGH);
       delayMicroseconds(stepperSpeed);
-      digitalWrite(pin_stepper[1], LOW);
+      digitalWrite(pin_stepper[i][1], LOW);
       delayMicroseconds(stepperSpeed);
     }
   }
@@ -49,7 +60,7 @@ void motor (int i, byte s) {
 }
 
 void setup() {
-
+  BT.begin(9600);
   Serial.begin(115200);
   for (int x = 0; x < 4; x++)
     for (int y = 0; y < 2; y++) {
@@ -62,8 +73,8 @@ void setup() {
       pinMode(pin_stepper[x][y], OUTPUT);
       digitalWrite(pin_stepper[x][y], LOW);
     }
-    for (int x = 0; x < 3; x++)
-  digitalWrite(pin_stepper[x][2], HIGH);
+  for (int x = 0; x < 3; x++)
+    digitalWrite(pin_stepper[x][2], HIGH);
   // put your setup code here, to run once:
 
 }
@@ -75,6 +86,8 @@ void loop() {
 
     switch (d) {
       case 0xD1:
+        byte temp_s_v[2];
+        Serial.readBytes(temp_s_v, 2);
         break;
 
       case 0xD2:
@@ -86,11 +99,14 @@ void loop() {
           if (temp_s_m[i] != 0 && count == 0)
             count++;
           else temp_s_m[i] = 0;
-          if (i < 3 && temp_s_m[i] != 0) {
-            stepper(i, temp_s_m[i]);
-          }
-          else if (i > 2 && temp_s_m[i] != 0) {
-            motor(i - 3, temp_s_m[i]);
+          if (temp_s_m[i] != s_m[i]) {
+            s_m[i] = temp_s_m[i];
+            if (i < 2 ) {
+              stepper(i, temp_s_m[i]);
+            }
+            else if (i > 1 && i < 7) {
+              motor(i - 2, temp_s_m[i]);
+            }
           }
         }
         break;
@@ -99,8 +115,24 @@ void loop() {
         Serial.write(0xF2);
         Serial.write(0xD1);
         break;
-        // put your main code here, to run repeatedly:
     }
   }
 
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (BT.available() > 20) {
+    //Serial.print(BT.readString());
+    //Serial.print("receive");
+    String a;
+    a = BT.readString();
+    //Serial.println(a);
+    if (a.indexOf('7')) {
+      int x = a.indexOf('7');
+      if (a[x + 1] == '/n')
+        a.remove(x, 1);
+      String BTinput = a.substring(x + 1, x + 8);
+      Serial.write(0xE1);
+      Serial.print(BTinput);
+    }
+  }
+  //] delay(10); // Delay for 10 ms so the next char has time to be received
 }
