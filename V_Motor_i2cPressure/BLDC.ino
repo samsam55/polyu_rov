8e0d89c37e73469df5718503c3d2a2414d4c3790
wan40657819 <wantakyin@gmail.com>
2018-03-03 20:15:51 +0800

    motor dir final

--- V_Motor_i2cPressure/BLDC.ino
+++ V_Motor_i2cPressure/BLDC.ino
@@ -1,5 +1,5 @@
 void BLDC_filterOutput() {
-  short V_BLDC_OUT[2] = {1500 + depth_Out + pitch_Out, 1500 + depth_Out - pitch_Out};
+  short V_BLDC_OUT[2] = {1500 - depth_Out + pitch_Out, 1500 + depth_Out + pitch_Out};
   
   for (byte i = 0; i < 2; i++) {
     if ( V_BLDC_OUT[i] - V_BLDC[i] >= 5 ) {
